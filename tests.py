bc6719a6131538b0234976490ccb75d40f7e06ab
陈玲琳 <chenlinglin@chenlinglindeMacBook-Pro.local>
2023-03-31 21:20:26 -0400

    further study

---
+++ tests.py
@@ -0,0 +1,57 @@
+"""Testsq for Balloonicorn's Flask app."""
+
+import unittest
+import party
+
+
+class PartyTests(unittest.TestCase):
+    """Tests for my party site."""
+
+    def setUp(self):
+        """Code to run before every test."""
+
+        self.client = party.app.test_client()
+        party.app.config['TESTING'] = True
+
+    def test_homepage(self):
+        """Can we reach the homepage?"""
+
+        result = self.client.get("/")
+        self.assertIn(b"having a party", result.data)
+
+    def test_no_rsvp_yet(self):
+        """Do users who haven't RSVPed see the correct view?"""
+
+        # FIXME: Add a test to show we haven't RSVP'd yet
+        result = self.client.get("/")
+        self.assertIn(b"Please RSVP",result.data)
+
+    def test_rsvp(self):
+        """Do RSVPed users see the correct view?"""
+
+        rsvp_info = {'name': "Jane", 'email': "jane@jane.com"}
+
+        result = self.client.post("/rsvp", data=rsvp_info,
+                                  follow_redirects=True)
+
+        # FIXME: check that once we log in we see party details--but not the form!
+        self.assertIn(b"Yay!", result.data)
+        self.assertIn(b"Party Details",result.data)
+        self.assertNotIn(b"Please RSVP",result.data)
+
+    def test_rsvp_mel(self):
+        """Can we keep Mel out?"""
+
+        # FIXME: write a test that mel can't invite himself
+        rsvp_info = {'name':"Mel", 'email':"mel@ubermelon.com"}
+        result = self.client.post("/rsvp", data=rsvp_info,
+                                  follow_redirects=True)
+
+        # FIXME: check that once we log in we see party details--but not the form!
+        self.assertNotIn(b"Yay!", result.data)
+        self.assertNotIn(b"Party Details",result.data)
+        self.assertIn(b"Please RSVP",result.data)
+
+
+if __name__ == "__main__":
+    unittest.main()
